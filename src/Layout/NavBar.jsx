305b6997b069a401248e704d82cdd2532a1618c4
mdjiaul-hoque <jiasofte@gmail.com>
2025-11-05 23:34:40 +0600

    reload off

--- src/Layout/NavBar.jsx
+++ src/Layout/NavBar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import { Outlet } from 'react-router'
+import { Link, Outlet } from 'react-router'
 
 const NavBar = () => {
   return (
     <nav>
+        
         <ul>
-            <li><a href="/">Home</a></li>
+            <li><Link to="/">Home</Link></li>
             {/* <li><a href="/product">Product</a></li> */}
-            <li><a href="/contact">Contact</a></li>
-            <li><a href="/about">About</a></li>
+            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/about">About</Link></li>
         </ul>
         <Outlet></Outlet>
     </nav>
