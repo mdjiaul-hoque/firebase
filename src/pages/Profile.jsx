05b7f084611f97327df41cfd497c4cfb152ca6ba
mdjiaul-hoque <jiasofte@gmail.com>
2025-11-06 18:33:58 +0600

    Programatically route and passing data/recive and show with usetLocation

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { useLocation } from 'react-router';
 
 const Profile = () => {
-const location = useLocation();
-console.log(location.state);
+const {state} = useLocation();
+console.log(state);
+
 
     return (
         <div className='pt-5'>
+            {state? (<>
             <h2 className='text-5xl font-bold'>User Profile</h2>
-            <p><span className='text-cyan-400'>Name:</span></p>
-            <p><span className='text-cyan-400'>Email:</span></p>
-            <p><span className='text-cyan-400'>City:</span></p>
-            <p><span className='text-cyan-400'>County:</span></p>
+            <p><span className='text-cyan-400 font-semibold'>Name:</span>{state.name}</p>
+            <p><span className='text-cyan-400 font-semibold'>Email:</span>{state.email}</p>
+            <p><span className='text-cyan-400 font-semibold'>City:</span>{state.city}</p>
+            <p><span className='text-cyan-400 font-semibold'>County:</span>{state.country}</p>
+            </>): <p>No Profile exist</p>}
         </div>
     );
 };
