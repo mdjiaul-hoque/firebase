d658ffaf406bbc8e9bb99713ccb1cdfb3d02870e
mdjiaul-hoque <jiasofte@gmail.com>
2025-11-06 19:54:27 +0600

    api fetch and passing and show with control eerror hendle

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,14 +3,24 @@ import React, { useEffect, useState } from 'react';
 const Product = () => {
     const [products , setProducts] = useState([]);
     const [isLoading , setIsLoading] = useState(false);
+    const [error , setError] = useState(null);
 
     useEffect(()=>{
         setIsLoading(true);
-
-        fetch('https://dummyjson.com/products').then((res)=>res.json()).then((data)=>{
+        setError(null);
+        fetch('https://dummyjson.com/products')
+        .then((res)=>{
+            if(!res.ok){
+                throw new Error("Data could not be fetched");
+                
+            }
+            return res.json()
+    })
+        .then((data)=>{
             setProducts(data.products);
             setIsLoading(false);
-        });
+        }).catch((error)=>setError(error.message))
+        .finally(()=>setIsLoading(false));
               
     },[])
     return (
@@ -23,11 +33,11 @@ const Product = () => {
                 {products && products.length>0 && products.map((product)=> {
                     const { id , title , category ,price, description} = product;
                     return  (
-                        <article className='border rounded-2xl p-4 my-4 bg-emerald-400 font-semibold text-black ' key={product.id}>
+                        <article className='border rounded-2xl p-4 my-4 border-r-gray-500 font-semibold  ' key={product.id}>
 
                             <h2 className='text-red-500'>{title}</h2>
-                            <h2>Catagory:{category}</h2>
-                            <h2>Price:{price}</h2>
+                            <h2>Catagory: {category}</h2>
+                            <h2>Price: {price}</h2>
                             <h2>{description && description.substring(0,100)}...</h2>
 
                         </article>
