b9616c69c465124bd2d950a780b514a2102a4186
mdjiaul-hoque <jiasofte@gmail.com>
2025-11-06 06:20:44 +0600

    Programatically route with useNavigate

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
 
 const Contact = () => {
+
+    const navigate = useNavigate();
+
+    const handleGoToHome = ()=>{
+        navigate('/');
+    };
+
     return (
         <div>
             <h1>contact</h1>
+            <button onClick={handleGoToHome}>go to home</button>
         </div>
     );
 };
